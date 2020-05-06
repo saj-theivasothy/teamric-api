ad2f1dac1bb7df3028d107a29fefbc9362f11688
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-05-06 17:42:56 -0400

    create db tables

---
+++ data/migrations/00_schema.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS person_assessments CASCADE;
+DROP TABLE IF EXISTS virtue_buckets CASCADE;
+DROP TABLE IF EXISTS virtues CASCADE;
+
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  job_title VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  isAdmin BOOLEAN
+);
+
+CREATE person_assessments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  employee_id INTEGER REFERENCES employees(id) NOT NULL,
+  survey_id INTEGER NOT NULL UNIQUE,
+  target_employee_id REFERENCES employees(id) NOT NULL
+)
+
+CREATE virtue_buckets (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(50) NOT NULL
+)
+
+CREATE virtues (
+  id SERIAL PRIMARY KEY NOT NULL,
+  virtue_bucket_id INTEGER REFERENCES virtue_buckets(id) NOT NULL,
+  name VARCHAR(50) NOT NULL UNIQUE,
+  description VARCHAR(255)
+)
+
+ALTER SEQUENCE employees_id_seq RESTART WITH 1;
+ALTER SEQUENCE person_assessments_id_seq RESTART WITH 1;
+ALTER SEQUENCE virtue_buckets_id_seq RESTART WITH 1;
+ALTER SEQUENCE virtues_id_seq RESTART WITH 1;
