fe396f8a65fb916361a7415e146b63e344fe5c60
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-05-13 23:06:03 +0000

    add getVirtues function to get all virtues from database

--- data/database.js
+++ data/database.js
@@ -1,1 +1,11 @@
 const db = require("./db/index.js");
+
+const getVirtues = () => {
+  const queryString = `
+    SELECT * FROM virtues;
+    `;
+
+  return db.query(queryString).then((res) => res.rows);
+};
+
+getVirtues();
