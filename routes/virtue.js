9d38ebf5e1abbeb74893d3bfd484977f608cd3f4
Saj <Saj@Sayinthans-MacBook-Pro.local>
2020-05-13 22:09:29 -0400

    fix error with buckets route

--- routes/virtue.js
+++ routes/virtue.js
@@ -1,5 +1,13 @@
 module.exports = function (router, database) {
-  router.get("/", (req, res) => {});
+  router.get("/", (req, res) => {
+    database.getVirtues().then((virtues) => res.send(virtues));
+  });
+
+  router.get("/buckets", (req, res) => {
+    database
+      .getVirtueBuckets()
+      .then((virtueBuckets) => res.send(virtueBuckets));
+  });
 
   return router;
 };
