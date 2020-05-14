90ac41386c16f6946ccad59c5aacfc4939cf5680
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-05-14 22:13:13 +0000

    add function to add survey data to database

--- data/database.js
+++ data/database.js
@@ -1,4 +1,6 @@
 const db = require("./db/index.js");
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017";
 
 const getVirtues = () => {
   const queryString = `
@@ -24,4 +26,33 @@ const getEmployees = () => {
   return db.query(queryString).then((res) => res.rows);
 };
 
-module.exports = { getVirtues, getVirtueBuckets };
+const getTest = () => {
+  db.collection.find().toArray((err, items) => {
+    console.log(items);
+  });
+};
+
+const addSurvey = (data) => {
+  MongoClient.connect(url, (err, client) => {
+    const db = client.db("nodetest1");
+
+    db.collection("usercollection").insertOne({ ...data });
+
+    client.close();
+  });
+};
+
+const data = {
+  employeeId: 3,
+  feedback: [
+    { skillId: 1, rating: 2, description: "blahblah" },
+    { skillId: 3, rating: 4, description: "hello" },
+    { skillId: 4, rating: 5, description: "byebye" },
+    { skillId: 2, rating: 4, description: "newdata" },
+    { skillId: 6, rating: 3, description: "newestdata" },
+    { skillId: 6, rating: 3, description: "newestdata2" },
+  ],
+};
+addSurvey(data);
+
+module.exports = { getVirtues, getVirtueBuckets, getEmployees };
